42f4367c2f40f84484ae4676e121b7df512d725c
Alexandru-Catalin GULYASY <alexandru.gulyasy@student.upt.ro>
2025-08-27 21:28:07 +0300

    init

---
+++ votingapp/src/main/java/com/voting/votingapp/model/OptionVote.java
@@ -0,0 +1,14 @@
+package com.voting.votingapp.model;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Embeddable
+public class OptionVote {
+    private String optionText;
+    private Long voteCount = 0L;
+}
